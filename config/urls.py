c85c8b7df267b2268ec964199970ddbec70de0a0
shahid-laskar <jtoitcelldev@gmail.com>
2024-11-14 17:02:07 +0530

    Day01

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test-app/', include('test_app.urls')),
+    path('employee-learning/', include('employee_learning.urls')),
 ]
